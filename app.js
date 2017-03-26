b0996a2cf462cf73bd1493adbeae7fe3e0c52e63
arielillouz <ariel.illouz@gmail.com>
2017-03-26 22:30:46 +0300

    a

--- app.js
+++ app.js
@@ -3,11 +3,14 @@
  */
 
 // https://metrics.librato.com/s/metrics/cls.api.in.latency?duration=21600&q=latency&source_filter=prod-svc-metrics-get_insights_%2A
+var d = new Date();
+ d.setDate(d.getDate()-1);
+
 
 $.ajax({
     type: "GET",
-    url: "https://metrics-api.librato.com/v1/metrics/cls.api.in.latency?count=20" +
-    "&resolution=60&source=prod-svc-metrics-get_insights_*",
+    url: "https://metrics-api.librato.com/v1/metrics/cls.api.in.latency?count=100" +
+    "&period=3600&resolution=60&source=prod-svc-metrics-get_insights_*",
     dataType: 'json',
     beforeSend: function (xhr) {
         xhr.setRequestHeader ("Authorization", "Basic " + btoa("dave.loper@cloudlock.com" + ":" + '0a597a0d16345e659d888650fccfdfde1f7db30004ae25bce2da90802a759509'));
@@ -20,7 +23,7 @@ $.ajax({
             series.push({
                 name: key,
                 data: _.map(data.measurements[key], function (m) {
-                    return [m.measure_time, m.value];
+                    return [m.measure_time * 1000, m.value];
                 })
             });
         });
@@ -42,11 +45,14 @@ $.ajax({
                     year: '%b'
                 },
                 title: {
-                    text: 'Date'
+                    visible: false
                 }
             },
             yAxis: {
-                min: 0
+                min: 0,
+                title: {
+                    visible: false
+                }
             },
             tooltip: {
                 headerFormat: '<b>{series.name}</b><br>',
